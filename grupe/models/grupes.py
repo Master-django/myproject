831001c1cbea6e639375409805f5f80812f09482
shofuzayl <shofuzayl2003@gmail.com>
2024-03-06 10:27:35 +0500

    commit

---
+++ grupe/models/grupes.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Grupe(models.Model):
+    name = models.CharField("Name", max_length=100, blank= True, null= True)
+    address = models.CharField("Suroga", max_length = 100)
+    number_phone = models.IntegerField("Raqami telefon")
+    burth_day = models.DateField("soli tavalud")
+    nation = models.CharField("Millat", max_length = 100)
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Груп'
+        verbose_name_plural = 'групы'
+
+        def __str__(self) -> str:
+            return self.name 
